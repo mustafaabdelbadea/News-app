cb2a9d2b76e3a61d2463b5776bb7834d96f9537b
mustafa <mustafa.abdelbadea@gmail.com>
2020-06-20 02:23:36 +0200

    component commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GeneralComponent } from './general/general.component';
+import { SportsComponent } from './sports/sports.component';
+import { NavComponent } from './nav/nav.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GeneralComponent,
+    SportsComponent,
+    NavComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
